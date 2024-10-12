9f6346320bc4fcaa26c63e13847f2138a1828df2
rcsilva96 <101437353+rcsilva96@users.noreply.github.com>
2024-10-12 16:12:38 -0300

    beta version

---
+++ assets/js/contato.js
@@ -0,0 +1,7 @@
+function telefone(){
+    alert("Nosso telefone é (11) XXXX-XXXX, aguardamos seu contato!")
+}
+
+function aviso(){
+    alert("Esse site ainda é uma versão de testes! Não está liberado para uso e está pendente de ajustes!")
+}
